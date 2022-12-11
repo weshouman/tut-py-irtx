351ccb5bae9073be2be87a3e19c80830970342db
Walid Shouman <eng.walidshouman@gmail.com>
2022-12-11 01:19:14 +0100

    Add tfidf computation and update requirements

---
+++ tests/test_tfidf.py
@@ -0,0 +1,111 @@
+import logging
+import unittest
+import xmlrunner
+
+import tut_py_irtx.tfidf as tfidf
+from tests.stub_lev_dist import *
+
+def setUpModule():
+  """Triggered before all module tests"""
+  logging.debug("setUpModule is triggered")
+
+def tearDownModule():
+  """Triggered after all module tests"""
+  logging.debug("tearDownModule is triggered")
+
+class TFIDFTest(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(cls):
+    """Triggered before all class tests"""
+    logging.debug("setUpModule is triggered")
+
+  def setUp(self):
+    """Triggered before each test"""
+    logging.debug("setUp is triggered")
+
+  def assert_tf(self, term_count, expected_tf):
+    tf = tfidf.calc_tf(term_count)
+    self.assertEqual(round(tf), round(expected_tf), f"Term Frequency of {term_count} should be {expected_tf}")
+
+  def test01_calc_tf(self):
+    """The term frequency is correctly computed"""
+
+    tfmul = tfidf.TF_MULTIPLIER
+    self.assert_tf(-1000, 0)
+    self.assert_tf(0, 0)
+    self.assert_tf(1,    1     * tfmul)
+    self.assert_tf(10,   2     * tfmul)
+    self.assert_tf(15,   2.176 * tfmul)
+    self.assert_tf(100,  3     * tfmul)
+    self.assert_tf(370,  3.568 * tfmul)
+    self.assert_tf(1000, 4     * tfmul)
+    self.assert_tf(4000, 4.602 * tfmul)
+
+  def assert_idf(self, docs, total_docs, expected_idf):
+    idf = tfidf.calc_idf(docs, total_docs)
+    self.assertEqual(round(idf), round(expected_idf), f"Term Frequency of {docs}/{total_docs} should be {expected_idf}")
+
+  def test02_calc_idf(self):
+    """The inverted document frequency is correctly computed"""
+
+    idfmul = tfidf.IDF_MULTIPLIER
+
+    with self.assertRaises(ValueError):
+      tfidf.calc_idf(1001, 1000)
+
+    self.assert_idf(1000, 1000, 0)
+    self.assert_idf(100,  1000, 1     * idfmul)
+    self.assert_idf(10,   1000, 2     * idfmul)
+    self.assert_idf(5,    1000, 2.301 * idfmul)
+    self.assert_idf(1,    1000, 3     * idfmul)
+
+  def test03_calc_similarity(self):
+    """The cosine similarity is correctly computed"""
+    # using tfmul and idfmul do not do any difference,
+    #   as similarity function would normalize
+    # it is only used here to match with real scenarios
+    tfmul = tfidf.TF_MULTIPLIER
+    idfmul = tfidf.IDF_MULTIPLIER
+
+    # query -> home sweet home
+    qtf  = [1 * tfmul, 2 * tfmul]
+    # the query resembles a single document, no rarity difference
+    qidf = [1 * idfmul, 1 * idfmul]
+    # document -> has both the words only once 
+    dtf  = [1 * tfmul, 1 * tfmul]
+    # sweet is rarer than home 
+    didf = [2 * idfmul, 3.3 * idfmul]
+
+    similarity = tfidf.get_query_similarity(qtf, qidf, dtf, didf)
+    self.assertEqual(round(similarity, 4), 0.9967)
+
+    # sweet is as rare as home 
+    didf = [2 * idfmul, 2 * idfmul]
+
+    similarity = tfidf.get_query_similarity(qtf, qidf, dtf, didf)
+    self.assertEqual(round(similarity, 4), 0.9487)
+
+    # document -> has `sweet` twice as much it has `home``
+    dtf  = [1 * tfmul, 2 * tfmul]
+
+    similarity = tfidf.get_query_similarity(qtf, qidf, dtf, didf)
+    self.assertEqual(round(similarity, 7), 1)
+
+  def tearDown(self):
+    """Triggered after each test"""
+    logging.debug("tearDown is triggered")
+
+  @classmethod
+  def tearDownClass(cls):
+    """Triggered  after all class tests"""
+    logging.debug("tearDownClass is triggered")
+
+
+# if __name__ == '__main__':
+#     unittest.main(
+#         testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
+#         # these make sure that some options that are not applicable
+#         # remain hidden from the help menu.
+#         failfast=False, buffer=False, catchbreak=False)
+
---
+++ tut_py_irtx/tfidf.py
@@ -0,0 +1,84 @@
+import logging
+import math
+import numpy as np
+
+TF_MULTIPLIER  = 10
+IDF_MULTIPLIER = 10
+
+def calc_tf(term_count):
+  """
+  Returns
+  ---------
+  tf: int
+    Term Frequency, a measure for how frequent a term in a document is.
+    0: if document does not have the word.
+    [1+log(countOfTermAppearanceInDoc)]*1000: if document has the word.
+    The value is multiplied by 1000 to use integers instead of floats.
+  """
+  if term_count < 1:
+    return 0
+  else:
+    # return round((1 + math.log10(term_count)) * TF_MULTIPLIER)
+    return (1 + math.log10(term_count)) * TF_MULTIPLIER
+
+def calc_idf(docs, total_docs):
+  """
+  Returns
+  -------
+  idf: int
+    Inverted Document Frequency, a measure of how rare a word is
+    getting the log(totalDocs/docsThisTermAppearedIn)
+
+    Note: The idf is different from calculating based on the 
+    Collection Frequency, which is log(totalTokens/countOfToken).
+    Calculation based on the collection frequency does not give 
+    a real measure of rarity, as a generic word that's common
+    between many docs would would give a similar result to a word
+    that is a topic-based common, and is usually used a lot when
+    talking about a specific topic.
+  """
+  if docs == 0:
+    raise ValueError(f"[IDF] A term appeared in 0 docs, should not be in the dictionary")
+    return 0
+  elif total_docs < docs:
+    #raise ValueError(f"[IDF] Total_docs({total_docs}) is less than docs({docs})")
+    raise ValueError()
+  else:
+    # return round((math.log10(total_docs/docs)) * IDF_MULTIPLIER)
+    return (math.log10(total_docs/docs)) * IDF_MULTIPLIER
+
+def get_query_similarity(qtf_list, qidf_list, dtf_list, didf_list, tfweight=1, idfweight=1):
+  """Calculate cosine similarity.
+
+  Even though the similarity is calculated between queries and documents
+  this method could be used to get the similarity between documents.
+
+  Parameters
+  ----------
+  qtf_list : list of int
+    term frequencies of query elements
+  qidf_list : list of int 
+    inverted document frequencies of query elements
+  dtf_list : list of int
+    term frequencies of document elements
+  didf_list : list of int 
+    inverted document frequencies of document elements
+  """
+  # Prepare arrays
+  qtf  = np.array(qtf_list)
+  qidf = np.array(qidf_list)
+  dtf  = np.array(dtf_list)
+  didf = np.array(didf_list)
+
+  q = qtf * qidf
+  d = dtf * didf
+
+  qd = np.dot(q, d)
+  qlen = np.sqrt(np.dot(q, q))
+  dlen = np.sqrt(np.dot(d, d))
+
+  logging.debug(f"\n[q:{q}] \t[d:{d}] \t[qd:{qd}] \t[qlen is {qlen}] \t[dlen is {dlen}]")
+
+  similarity = qd / (qlen * dlen)
+
+  return similarity
